4527b0ecc99fbea7eec9b01b7706c88a4cbb32fc
Princetonkaberia <princeton.kaberia@strathmore.edu>
2026-06-04 13:27:09 +0300

    lab 2.7 js 101

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+let age = 20;
+let course = "BBIT";
+const university = "Strathmore University";
+
+
+let age = 27;
+let course = "ECONOMICS STATISTICS";
+const university = "USIU";
+ if (true) {
+    let time = "11:30";
+    var day = "Monday";
+    const month = "June";
+   
+}
+console.log (time);
