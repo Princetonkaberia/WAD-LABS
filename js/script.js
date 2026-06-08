3d5acaa508c153e0fe0cdc5ca6b5a3e285c8eab9
Princetonkaberia <princeton.kaberia@strathmore.edu>
2026-06-08 13:04:01 +0300

    functionslab 2.8 js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,44 @@
 
-let age = 20;
-let course = "BBIT";
-const university = "Strathmore University";
+//let age = 20;
+//let course = "BBIT";
+//const university = "Strathmore University";
 
 
-let age = 27;
-let course = "ECONOMICS STATISTICS";
-const university = "USIU";
- if (true) {
-    let time = "11:30";
-    var day = "Monday";
-    const month = "June";
+//let age = 27;
+//let course = "ECONOMICS STATISTICS";
+//const university = "USIU";
+ //if (true) {
+    //let time = "11:30";
+    //var day = "Monday";
+    //const month = "June";
    
+
+//console.log (time);
+/**
+ * A funtion that ccalulates the area of a rectangle
+ * @param {int} height 
+ * @param {int} width 
+ * @returns {int} area of a rectangle
+ */
+
+//function declaration
+function calculateArea(height , width) {
+    let area = height * width
+    return area
+    //console.log("function calculateArea is called")
+    //alert("function called/executed");
 }
-console.log (time);
+
+// execution - called
+calculateArea(20,4) // a function call
+console.log(calculateArea(20,4)) 
+console.log(calculateArea(20))
+console.log(calculateArea())
+const add = funtion add(number_1,number_2) 
+ return number_1 + number_2
+}
+console.log(add(10,20))
+// arrow function
+const add = (number_1,number_2) => {
+    return number_1 + number_2
+}
